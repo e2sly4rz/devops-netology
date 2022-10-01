2062bedd10a8f0ede92dd099885dfdda96365237
e2sly4rz <95619056+e2sly4rz@users.noreply.github.com>
2022-10-01 17:51:22 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "yandex_cloud_id" {
+  default = "b1gap55cri0lll6m7tdo"
+}
+
+variable "yandex_folder_id" {
+  default = "b1gntb1dnbfkdlpvmf67"
+}
+
+variable "ubnt_ver" {
+  default = "fd8autg36kchufhej85b"
+}
